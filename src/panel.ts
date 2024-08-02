084840c61bfec64b54100261f8ae6a8954233065
Hamidreza Mohaqeq <mohaqeq@outlook.com>
2024-08-02 09:50:41 +0200

    Change panel

--- src/panel.ts
+++ src/panel.ts
@@ -52,7 +52,7 @@ export async function GetPanel(request: Request, env: Env): Promise<Response> {
     if (hash) {
       passwordSection = `
       <div class="mb-3 p-1">
-        <button type="submit" name="reset_password" value="1" class="btn btn-danger">Remove Password/button>
+        <button type="submit" name="reset_password" value="1" class="btn btn-danger">Remove Password</button>
       </div>
       `
     } else {
@@ -405,20 +405,6 @@ export async function GetPanel(request: Request, env: Env): Promise<Response> {
           <button type="submit" id="save-button" name="save" value="save" class="btn btn-primary"></button>
           <button type="submit" id="reset-button" name="reset" value="reset" class="btn btn-warning"></button>
         </form>
-        <div class="p-1 border-top border-2 border-primary">
-          <div class="text-nowrap fs-6">
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none" role="img" class="octicon">
-              <g clip-path="url(#clip0_1668_3024)">
-                <path d="M9.52217 6.77143L15.4785 0H14.0671L8.89516 5.87954L4.76437 0H0L6.24656 8.8909L0 15.9918H1.41155L6.87321 9.78279L11.2356 15.9918H16L9.52183 6.77143H9.52217ZM7.58887 8.96923L6.95596 8.0839L1.92015 1.03921H4.0882L8.15216 6.7245L8.78507 7.60983L14.0677 14.9998H11.8997L7.58887 8.96957V8.96923Z" fill="currentColor"></path>
-              </g>
-              <defs>
-                <clipPath id="clip0_1668_3024">
-                  <rect width="16" height="16" fill="white"></rect>
-                </clipPath>
-              </defs>
-            </svg>
-          </div>
-        </div>
       </div>
     </body>
     </html>
@@ -465,20 +451,6 @@ export async function GetPanel(request: Request, env: Env): Promise<Response> {
                   <a id="open-variables-text" href="https://dash.cloudflare.com/?to=/:account/workers/services/view/${url.hostname.split(".")[0]}/production/settings/bindings" target="_blank"></a>
                 </li>
               </ol>
-            </div>
-            <div class="p-1 border-top border-2 border-primary">
-              <div class="text-nowrap fs-6">
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none" role="img" class="octicon">
-                  <g clip-path="url(#clip0_1668_3024)">
-                    <path d="M9.52217 6.77143L15.4785 0H14.0671L8.89516 5.87954L4.76437 0H0L6.24656 8.8909L0 15.9918H1.41155L6.87321 9.78279L11.2356 15.9918H16L9.52183 6.77143H9.52217ZM7.58887 8.96923L6.95596 8.0839L1.92015 1.03921H4.0882L8.15216 6.7245L8.78507 7.60983L14.0677 14.9998H11.8997L7.58887 8.96957V8.96923Z" fill="currentColor"></path>
-                  </g>
-                  <defs>
-                    <clipPath id="clip0_1668_3024">
-                      <rect width="16" height="16" fill="white"></rect>
-                    </clipPath>
-                  </defs>
-                </svg>
-              </div>
             </div>
           </div>
         </body>
